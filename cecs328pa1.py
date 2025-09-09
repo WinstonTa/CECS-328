a321f26d1e7c7eb17a57e5d8b1e2e858fc12dd44
Winston Ta <winstonnta@gmail.com>
2025-09-08 19:27:58 -0700

    first working version

---
+++ cecs328pa1.py
@@ -0,0 +1,31 @@
+# Winston Ta and Mindy Yun
+
+import csv
+from collections import Counter
+
+def actor_movie_counts(last_watched_file, actors_file):
+    # step 1
+    movies_to_actors = {}
+    with open(actors_file, newline='', encoding="utf-8") as file:
+        data = csv.DictReader(file)
+        for row in data:
+            movie = row["Movie"].strip()
+            actor = row["Actor"].strip()
+            if movie not in movies_to_actors:
+                movies_to_actors[movie] = []
+            movies_to_actors[movie].append(actor)
+    
+    actor_counts = Counter()
+    with open(last_watched_file, newline='', encoding='utf-8') as file:
+        data = csv.DictReader(file)
+        for row in data:
+            movie = row["Movie"].strip()
+            if movie in movies_to_actors:
+                for actor in movies_to_actors[movie]:
+                    actor_counts[actor] += 1
+    
+    for actor, count in actor_counts.items():
+        if count > 1:
+            print(f"{actor}: {count}")
+
+actor_movie_counts("last_watched.csv", "actors.csv")
