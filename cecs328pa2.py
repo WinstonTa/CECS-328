45b5d6a973f4f41229af4f8db7eb2327a8c5bd2f
Winston Ta <winstonnta@gmail.com>
2025-09-22 18:01:51 -0700

    cecs328pa2 initial start

---
+++ cecs328pa2.py
@@ -0,0 +1,23 @@
+# Winston Ta and Mindy Yun
+
+from sympy import Symbol, log, limit, simplify, oo
+import sympy as sp
+
+# symbolic input size
+n = sp.symbols('n')
+
+def same_asymptotic(left_function, right_function):
+    # asymptotic runtime check
+    same_runtime = False
+
+    # simplify f1 and f2 so that only highest ordered term is preserved
+    f1_compare = sp.simplify(left_function)
+    f2_compare = sp.simplify(right_function)
+
+    # compare function runtimes with a limit
+    ratio = sp.limit(f1_compare / f2_compare, n, oo)
+
+    print(ratio)
+    same_runtime = True if ratio.is_finite and ratio != 0 else False
+    
+    return same_runtime
